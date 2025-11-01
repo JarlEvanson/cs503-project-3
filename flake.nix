8ae34e344e23f61c0f5c8014e42c7cfa1c6a9370
Jarl Evanson <evanson.jarl@gmail.com>
2025-11-01 00:28:34 +0000

    Create `README.md`, `.gitignore`, and Nix flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    devShells."x86_64-linux".default = pkgs.mkShellNoCC {
+      packages = [
+        pkgs.ghc
+      ];
+    };
+  };
+}
